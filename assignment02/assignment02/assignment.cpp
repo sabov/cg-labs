a4b4a14ff7095713d176cb7cee74c92a52f9baf0
sabov <alexsabov@gmail.com>
2013-10-30 23:19:48 +0100

    Fix rotate function

--- assignment02/assignment02/assignment.cpp
+++ assignment02/assignment02/assignment.cpp
@@ -3,6 +3,7 @@
  */
  
 #include "assignment.h"
+#define PI 3.14159265
 using namespace std;
 
 // add your team members names and matrikel numbers here:
@@ -26,6 +27,7 @@ glm::mat4 glm_translate(float x, float y, float z) {
 }
 
 glm::mat4 glm_rotate(float a, glm::vec3 vector) {
+    a = a * PI / 180;
 	float rotationMatrixZ [16] = {cos(a), sin(a), 0, 0, -sin(a), cos(a), 0, 0, 0, 0, 1, 0, 0, 0, 0, 1};
 	return glm::make_mat4(rotationMatrixZ);
 }
@@ -67,19 +69,17 @@ void drawScene(int scene, float runTime) {
     //lines
     glm::mat4 lineS = glm_scale(0.01f, 0.03f, 1.0f);
     for(i = 1; i < 37; i++) {
-        drawCircle(white, glm_rotate(i * 9.0f, glm::vec3(0,0,1)) * lineS * glm_translate(-76.0f, 0.0f, 0.0f));
+        drawCircle(white, glm_rotate(i * 12.0f - 6.0f, glm::vec3(0,0,1)) * lineS * glm_translate(-76.0f, 0.0f, 0.0f));
     }
 
     //cars
-    glm::mat4 carsS = glm::scale(0.02f, 0.1f, 1.0f);
+    glm::mat4 carsS = glm_scale(0.02f, 0.1f, 1.0f);
     if(t > 360) {
         t = 0;
     }
     drawCircle(yellow, glm_rotate(t*2, glm::vec3(0,0,1)) * carsS * glm_translate(-35.5f, 0.0f, 0.0f));
     drawCircle(red, glm_rotate(t, glm::vec3(0,0,1)) * carsS * glm_translate(-40.0f, 0.0f, 0.0f));
     t++;
-
-   
 }
 
 
